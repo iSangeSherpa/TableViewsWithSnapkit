a594140914b87e015cbf5af98a9fbea8d56a580a
Sange Sherpa <sherpasange1215@gmail.com>
2023-05-11 21:29:59 +0545

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Sange Sherpa on 11/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
